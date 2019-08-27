76f63bd69b16575b735b66f2e23ef77c2e5fffed
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-08-27 21:18:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   def name=(dog_name)
-    this_dogs_names = dog_name
+    @this_dogs_names = dog_name
   end
   def name
-    this_dogs_names
+    @this_dogs_names
   end
 end
 lassie = Dog.new
